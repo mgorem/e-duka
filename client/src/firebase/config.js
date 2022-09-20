2065051887a96f084862e544d93e27d626668072
callmemg <oremgitonga22@gmail.com>
2022-09-20 13:08:03 +0300

    configure firebase auth,firestore-db,storage,loader-dom

---
+++ client/src/firebase/config.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyBo0e60jWRRdHA6E4BRGpbCCIGKryzLVxo",
+  authDomain: "mgsfineline.firebaseapp.com",
+  databaseURL: "https://mgsfineline-default-rtdb.firebaseio.com",
+  projectId: "mgsfineline",
+  storageBucket: "mgsfineline.appspot.com",
+  messagingSenderId: "794676245747",
+  appId: "1:794676245747:web:4b46381ddd35ca392e7386",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
