7c0910d444be36a47d132c0682a5da61784bb0f5
callmemg <oremgitonga22@gmail.com>
2022-09-20 13:57:36 +0300

    register user with email and password firebase

--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -1,20 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "./Auth.module.scss"
 import registerImg from '../../assets/register.png'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import Card from '../../components/card/Card'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { auth } from "../../firebase/config"
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import Loader from "../../components/loader/Loader"
 
 const Register = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [cPassword, setCPassword] = useState("")
+  const [isLoading, setIsLoading] = useState(false)
+
+  const navigate = useNavigate()
+
+  const registerUser = (e) => {
+    e.preventDefault()
+    if (password !== cPassword) {
+      toast.error("Passwords do not match")
+    }
+    
+    setIsLoading(true)
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user)
+        setIsLoading(false)
+        toast.success("Registration Successful...")
+        navigate("/login")
+      })
+      .catch((error) => {
+        toast.error(error.message)
+        setIsLoading(false)
+      });
+  }
+
   return (
+    <>
+    <ToastContainer />
+    {isLoading && <Loader />}
     <section className={`container ${styles.auth}`}>
       <Card>
      <div className={styles.form}>
       <h2>Register</h2>
-      <form>
-        <input type="text" placeholder='Email' required />
-        <input type="Password" placeholder='Password' required />
-        <input type="Password" placeholder='Confirm Password' required />
-        <button className="--btn --btn-primary --btn-block">Register</button>
+      <form onSubmit={registerUser}>
+        <input type="text" placeholder='Email' required value={email} onChange={(e) => setEmail(e.target.value)}/>
+        <input type="Password" placeholder='Password' required value={password} onChange={(e) => setPassword(e.target.value)} />
+        <input type="Password" placeholder='Confirm Password' required value={cPassword} onChange={(e) => setCPassword(e.target.value)} />
+        <button type="submit" className="--btn --btn-primary --btn-block">Register</button>
       </form>
       <span className={styles.register}>
         <p>Already have an account?</p>
@@ -26,6 +63,7 @@ const Register = () => {
       <img src={registerImg} alt="Login" width={400} />
      </div>
     </section>
+    </>
   )
 }
 
